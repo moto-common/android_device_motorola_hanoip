6c4fc1cfdd945e507bdcca5a5ea8ef10e8e4baf8
electimon <electimon@gmail.com>
2024-01-07 10:16:53 +0000

    hanoip: Disable display idle timer
    * It will cause DSI to fail to turn the display off
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I245872ac1452df717787dc4210b39d756f5ab931

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ AB_OTA_PARTITIONS += \
 # Device characteristics
 DEVICE_CHARACTERISTICS := hfr
 
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.idle_time=0
+
 # Fingerprint
 TARGET_USES_CHIPONE_FINGERPRINT := true
 TARGET_USES_FPC_FINGERPRINT := true
